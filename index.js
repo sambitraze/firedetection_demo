38c2d40446440d6a19b2eee567e3981833287c40
SAMBIT MAJHI <majhisambit2@gmail.com>
2021-05-18 22:23:32 +0530

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3000, () =>
+  console.log(`fire detection Server is running on port 3000`)
+);
