e4310a558604e1b06ab5146ad2ef5abda759d6cc
SAMBIT MAJHI <majhisambit2@gmail.com>
2021-05-18 22:44:15 +0530

    ssl

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 const cors = require("cors");
 
+const https = require("https");
+
 const app = express();
 
 app.use(cors());
@@ -9,6 +11,19 @@ app.use(cors());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(3000, () =>
-  console.log(`fire detection Server is running on port 3000`)
-);
+// app.listen(3000, () =>
+//   console.log(`fire detection Server is running on port 3000`)
+// );
+
+const httpsServer = https.createServer(
+    {
+      key: fs.readFileSync(path.resolve(__dirname,'./ssl/privkey.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname,'./ssl/fullchain.pem'))
+    },
+    app
+  );
+
+
+  httpsServer.listen(443, () => {
+    console.log("HTTPS Server running on port 443");
+  });
